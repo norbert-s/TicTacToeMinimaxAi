8ec286596a41610fa14e4fc10902223e7011be10
Susztek Norbert <susztek.norbert@gmail.com>
2023-05-02 12:07:24 +0200

    updated

--- src/ComputerPlayer.java
+++ src/ComputerPlayer.java
@@ -1,10 +1,11 @@
+
 public class ComputerPlayer {
     private String symbol;
     private AI ai;
 
     public ComputerPlayer(String symbol, String opponentSymbol) {
         this.symbol = symbol;
-        this.ai = new AI(symbol, opponentSymbol);
+        this.ai=new AI(symbol,opponentSymbol);
     }
 
     public int makeMove(String[][] arr) {
